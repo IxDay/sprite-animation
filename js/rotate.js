fa6fe727ecdc071d1c1d3be85f4aef51bd5588c1
IxDay <maxime.vidori@gmail.com>
2013-01-24 23:25:38 +0100

    The sprites turns

--- js/rotate.js
+++ js/rotate.js
@@ -1,89 +1,70 @@
-/*|
-|*|
-|*|  IE-specific polyfill which enables the passage of arbitrary arguments to the
-|*|  callback functions of javascript timers (HTML5 standard syntax).
-|*|
-|*|  https://developer.mozilla.org/en-US/docs/DOM/window.setInterval
-|*|
-|*|  Syntax:
-|*|  var timeoutID = window.setTimeout(func, delay, [param1, param2, ...]);
-|*|  var timeoutID = window.setTimeout(code, delay);
-|*|  var intervalID = window.setInterval(func, delay[, param1, param2, ...]);
-|*|  var intervalID = window.setInterval(code, delay);
-|*|
-|*/
-
-if (document.all && !window.setTimeout.isPolyfill) {
-    var __nativeST__ = window.setTimeout;
-    window.setTimeout = function (vCallback, nDelay /*, argumentToPass1, argumentToPass2, etc. */) {
-        var aArgs = Array.prototype.slice.call(arguments, 2);
-        return __nativeST__(vCallback instanceof Function ? function () {
-            vCallback.apply(null, aArgs);
-        } : vCallback, nDelay);
-    };
-    window.setTimeout.isPolyfill = true;
-}
-
-if (document.all && !window.setInterval.isPolyfill) {
-    var __nativeSI__ = window.setInterval;
-    window.setInterval = function (vCallback, nDelay /*, argumentToPass1, argumentToPass2, etc. */) {
-        var aArgs = Array.prototype.slice.call(arguments, 2);
-        return __nativeSI__(vCallback instanceof Function ? function () {
-            vCallback.apply(null, aArgs);
-        } : vCallback, nDelay);
-    };
-    window.setInterval.isPolyfill = true;
-}
-
-
-var Sprite = function(url,size,time,vertical,element){
+var Sprite = function (url, size, time, element) {
     var that = this;
 
-    that.vertical_ = vertical;
     that.size_ = size;
     that.time_ = time;
     that.element_ = element;
+    that.updateXY(0,0);
 
     var img = new Image();
     img.src = url;
 
-    that.loaded_ = function(callback){
-        img.onload = function() {
+    that.loaded_ = function (callback) {
+        img.onload = function () {
             that.height_ = this.height;
             that.width_ = this.width;
-            if(callback){
-                callback(that);
+            that.vertical_ = that.height_ > that.width_;
+
+            var width = that.width_ + "px";
+            var height = that.height_ + "px";
+            var size = that.size_ + "px";
+
+            if (that.vertical_) {
+                that.element_.style.width = width;
+                that.element_.style.height = size;
+            } else {
+                that.element_.style.width = size;
+                that.element_.style.height = height;
+            }
+
+            that.element_.style.background = "url(" + url + ")";
+
+
+            if (callback) {
+                callback();
             }
         };
-    }
+    };
 };
 
-Sprite.prototype.start = function(){
-    this.loaded_(function(elem){
-        console.log(elem);
-    })
+Sprite.prototype.updateXY = function(x,y){
+    this.x_ = x;
+    this.y_ = y;
+    this.element_.style.backgroundPosition = this.x_ + "px " + this.y_ + "px";
 };
 
-var sprite = document.getElementsByClassName('sprite')[0];
-var toto = new Sprite('http://photos.carapuce.12.lc/img/rotate360.jpg',376,1000,true,sprite);
-toto.start();
+Sprite.prototype.start = function () {
+    var that = this;
+    that.loaded_(function () {
+        that.loop_ = window.setInterval(function () {
+            var offset;
+            if(that.vertical_){
+                offset = that.y_ + that.size_;
+                that.y_ = offset > that.height_ ? 0 : offset;
+            }else{
+                offset = that.x_ + that.size_;
+                that.x_ = offset > that.width_ ? 0 : offset;
+            }
+            that.updateXY(that.x_,that.y_);
+        }, that.time_);
 
-window.onload = function(){
-    var sprites = document.getElementsByClassName('sprite');
+    });
+};
 
-//    window.addEventListener('mouseover',function(){
-//
-//    });
 
-    for(var i = 0, l = sprites.length; i < l; i++){
-        var sprite = sprites[i];
-        var style = sprite.currentStyle || window.getComputedStyle(sprite, null);
-//        console.log(style.backgroundImage.slice(4, -1));
-//
-//        window.getComputedStyle(sprite,null).getPropertyValue('background');
+window.onload = function () {
 
-//        window.setInterval(function(sprite){
-//            console.log();
-//        },1000,sprites[i]);
-    }
+    var sprite = new Sprite('http://192.168.1.41/img/rotate360.jpg', 376, 10,
+        document.getElementById('sprite'));
+    sprite.start();
 };
