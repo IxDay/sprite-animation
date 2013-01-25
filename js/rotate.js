4d8969d8b8f34853f102aaf8808a36a045564345
IxDay <maxime.vidori@gmail.com>
2013-01-25 11:04:18 +0100

    alpha release

--- js/rotate.js
+++ js/rotate.js
@@ -1,70 +1,17 @@
-var Sprite = function (url, size, time, element) {
-    var that = this;
 
-    that.size_ = size;
-    that.time_ = time;
-    that.element_ = element;
-    that.updateXY(0,0);
-
-    var img = new Image();
-    img.src = url;
-
-    that.loaded_ = function (callback) {
-        img.onload = function () {
-            that.height_ = this.height;
-            that.width_ = this.width;
-            that.vertical_ = that.height_ > that.width_;
-
-            var width = that.width_ + "px";
-            var height = that.height_ + "px";
-            var size = that.size_ + "px";
-
-            if (that.vertical_) {
-                that.element_.style.width = width;
-                that.element_.style.height = size;
-            } else {
-                that.element_.style.width = size;
-                that.element_.style.height = height;
-            }
-
-            that.element_.style.background = "url(" + url + ")";
-
-
-            if (callback) {
-                callback();
-            }
-        };
-    };
-};
-
-Sprite.prototype.updateXY = function(x,y){
-    this.x_ = x;
-    this.y_ = y;
-    this.element_.style.backgroundPosition = this.x_ + "px " + this.y_ + "px";
-};
-
-Sprite.prototype.start = function () {
-    var that = this;
-    that.loaded_(function () {
-        that.loop_ = window.setInterval(function () {
-            var offset;
-            if(that.vertical_){
-                offset = that.y_ + that.size_;
-                that.y_ = offset > that.height_ ? 0 : offset;
-            }else{
-                offset = that.x_ + that.size_;
-                that.x_ = offset > that.width_ ? 0 : offset;
-            }
-            that.updateXY(that.x_,that.y_);
-        }, that.time_);
-
-    });
-};
 
 
 window.onload = function () {
 
-    var sprite = new Sprite('http://192.168.1.41/img/rotate360.jpg', 376, 10,
+    sprite = new photos.Sprite('http://photos.carapuce.12.lc/img/rotate360.jpg', 376, 50,
         document.getElementById('sprite'));
-    sprite.start();
+
+    document.getElementById('stop').addEventListener('click', function () {
+        sprite.stop();
+    });
+
+    document.getElementById('start').addEventListener('click', function () {
+        sprite.start();
+    });
+
 };
